56dde93a43cb29a3cd7e86c3e3c6b3d0d34c8071
Chris Dorward <listingslab@gmail.com>
2020-12-26 17:02:44 +1000

    Refactors some things

--- functions/endpoints/user/index.js
+++ functions/endpoints/user/index.js
@@ -1,24 +1,12 @@
-
-
-// user
-
 exports.getUser = async (req, res, db, userId) => {
-
-	/*
-		email: `listingslab@gmail.com`,
-		avatar: `https://listingslab.com/wp-content/uploads/2020/11/Octocat.png`,
-		displayName: `Octocat`
-	*/
-
 	const cityRef = db.collection('users').doc(userId)
 	const doc = await cityRef.get()
 	if (!doc.exists) {
 		return { 
 			error: true,
-			message : `Get User Error. User ID doesn't exist`,
+			message : `Error getting user. ID doesn't exist`,
 			userId,
-		}
-			
+		}		
 	} else {
 	  return {
 		  	uid: userId,
