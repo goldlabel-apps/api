3e882e45408abfcd1c16353a57c2823931750d99
Chris Dorward <listingslab@gmail.com>
2021-03-30 19:32:27 +1000

    10.0.9

--- functions/endpoints/pingpong/index.js
+++ functions/endpoints/pingpong/index.js
@@ -1,28 +1,51 @@
 exports.pingpong = async (req, res, db) => {	
-
+	/* TAKES FINGERPRINT, returns ID */
 	const {
 		method,
-		query,
-		path,
-		params,
+		body,
 	} = req
+	const { fingerprint } = body
+	if ( method !== 'POST'){ return { status: 601, data: {	message: `POST verb is required` } } }
+	if ( !fingerprint ){ return { status: 602, data: {	message: `fingerpring is required` } } }
 
+	let id = ``
+	const tingQuery = await db.collection(`pingpong`)
+						.where(`fingerprint`, `==`, fingerprint)
+						.get()	
+
+	if (tingQuery.empty){
+		const newTing = await db.collection(`pingpong`).add({
+			created: Date.now(),
+			...body,
+		})
+		id = newTing.id
+	} else {
+		tingQuery.forEach((doc) => { 
+			id = doc.id
+		})
+		const updateTing = await db.collection(`pingpong`)
+									.doc(id)
+									.set({
+										...body,
+									}, { merge: true })
+	}
 	return {
 		status: 200,
-		data: {
-			message: `PingPong all good, bruv`,
-		},
+		data: { id }	
 	}
-
 }
 
 
 
 
 
-	// if ( verb !== `GET` ){
-	// 	return {
-	// 		error: true, status: 601,
-	// 		data: { message: `Request method must be GET` }
-	// 	}
+
+
+/*
+	// let params = req.params[0].split( `/` )
+	// params = params.slice( 1, params.length)
+	// const action = params[1]
+	// if ( action === `update` ){
+	// 	console.log('Is there a record for the fingerprint blah in the pingpong collection?')
 	// }
+*/
