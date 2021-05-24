6923a987d8d5efe9856ea9624ccf440bf82c3cc8
listingslab <listingslab@gmail.com>
2021-05-24 20:17:51 +1000

    14.0.8

--- functions/endpoints/hosts/hosts.js
+++ functions/endpoints/hosts/hosts.js
@@ -2,14 +2,38 @@
 exports.hosts = async (req, res, db, action) => {	
 
 	if (action === 'first-run' ){
+		const {
+			body,
+			method,
+		} = req
+		const { url } = body
+		if ( method !== 'POST'){ return { status: 601, data: {	message: `POST verb is required` } } }
+		if ( !url ){ return { status: 602, data: {	message: `url is required` } } }
+		let id = ``
+		const hostsQuery = await db.collection(`hosts`)
+						.where(`url`, `==`, url)
+						.get()	
+		if (hostsQuery.empty){
+			const newHost = await db.collection(`hosts`).add({
+				created: Date.now(),
+				...body,
+			})
+			id = newTing.id
+		} else {
+			hostsQuery.forEach((doc) => { 
+				id = doc.id
+			})
+			const updateHost = await db.collection(`hosts`)
+										.doc(id)
+										.set({
+											...body,
+										}, { merge: true })
+		}				
 		return {
-		status: 200,
-			data: { 
-				message: `all good, brah`,
-			}	
+			status: 200,
+			data: { id }
 		}
 	}
-
 	return {
 		status: 500,
 		data: { 
